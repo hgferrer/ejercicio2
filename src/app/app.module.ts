7a2d24b711719be058381d8122f795701cb15f2f
hgferrer <hgferrer@gmail.com>
2022-03-23 15:41:19 -0300

    comienzo ejercicio_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditEmpleadoComponent } from './components/edit-empleado/edit-empleado.component';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { ListEmpleadoComponent } from './components/list-empleado/list-empleado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditEmpleadoComponent,
+    CreateEmpleadoComponent,
+    ListEmpleadoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
